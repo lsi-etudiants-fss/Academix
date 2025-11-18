b10550582e60d87e2e1c00fdcdb78f3729ba03d1
lsi-etudiants-fss <ibrahimaouichaoui4@gmail.com>
2025-11-18 20:25:17 +0100

    Create authService.js with authentication methods

---
+++ frontend/src/services/authService.js
@@ -0,0 +1,25 @@
+// authService.js
+
+const authService = {
+  signUp: async (userData) => {
+    // Logic for signing up a user
+  },
+
+  signIn: async (credentials) => {
+    // Logic for signing in a user
+  },
+
+  signOut: async () => {
+    // Logic for signing out a user
+  },
+
+  getCurrentUser: () => {
+    // Logic to get currently authenticated user
+  },
+
+  getSession: () => {
+    // Logic to get the current session
+  }
+};
+
+export default authService;
